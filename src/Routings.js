9f845cc6cdfbbd635105c117dad5e65d749c1144
yuktasharma098 <sharmayukta098@gmail.com>
2024-01-21 15:50:03 +0530

    commit

--- src/Routings.js
+++ src/Routings.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
 function Routings() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
-  
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
